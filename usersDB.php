80f0dff7a9e14fe8fd141a74a4d533ca9ed934b8
Ann75 <76436242+Anna45-75@users.noreply.github.com>
2022-05-26 02:58:44 +0300

    Update usersDB.php

--- usersDB.php
+++ usersDB.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    ['login' => 'admin', 'password' => 'P@ssw0rd'],
-    ['login' => 'Вася',  'password' => 'password'],
-    ['login' => 'Клава',  'password' => '123'],
+    ['login' => 'admin', 'password' => 'P@ssw0rd', 'date' =>'31.08.1975'],
+    ['login' => 'Вася',  'password' => 'password', 'date' =>'24.05.1975'],
+    ['login' => 'Клава',  'password' => '123', 'date' =>'26.05.1980'],
 ];
 ?>
