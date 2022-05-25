41ee7eb0bb41c234934f479ff7af485d98dfacd9
Ann75 <76436242+Anna45-75@users.noreply.github.com>
2022-05-26 02:59:26 +0300

    Update login.php

--- login.php
+++ login.php
@@ -8,29 +8,46 @@
   <link href="css/style.css" rel="stylesheet">
   <?php
 if (!empty($_POST)) {
+    /* подключение файла с массивом кук */
     require __DIR__ . '/auth.php';
 
-    $login = $_POST['login'] ?? '';
-    $password = $_POST['password'] ?? '';
+    $login = $_POST['login'];
+    $password = $_POST['password'];
+    $date = $_POST['date'];
+
+  /* верификация хэш */
     $hash = password_hash($passwod, PASSWORD_DEFAULT);
-    
     if (password_verify($password, $hash)) {
         true;
     }
     else {
         false;
     }
-
-    if (checkPassword($login, $password)) {
+ /* Проверка кук */
+    if (checkPassword($login, $password,$date)) {
         setcookie('login', $login, 0, '/');
         setcookie('password', $password, 0, '/');
+        setcookie('date', $date, 0, '/');
         header('Location: /index.php');
     } else {
         $error = 'Ошибка авторизации';
     }
-}
+};
+/* Функция проверки ДР */
+function aDate(){
+$born = strtotime($date);
+$now = time();
+$next_birthday = mktime(0, 0, 0, date('m',$born), date('d',$born), date('Y'));
 
+if ($next_birthday > $now && $next_birthday < strtotime('+5 day')) {
+$x = 1;
+} else {
+$x = 0;
+}
+echo 'У Вас праздничная скидка!';
+}
 ?>
+
 </head>
 <body>
   <header>
@@ -44,14 +61,20 @@ if (!empty($_POST)) {
 <?php endif; ?>
 <div class="row justify-content-center p-3">
     <div class="col-md-4">
+<!-- Форма входа -->
 <form action="login.php" method="post">
 <div class="mb-3">
-    <label for="login" class="form-label">Имя пользователя: </label><input type="text" name="login" id="login" class="form-control">
+    <label for="login" class="form-label">Имя пользователя: </label><input type="text" name="login" id="login" placeholder="Введите имя" class="form-control">
 </div>
 <div class="mb-3">
-    <label for="password" class="form-label">Пароль: </label><input type="password" name="password" id="password" class="form-control">
+    <label for="password" class="form-label">Пароль: </label><input type="password" name="password" id="password"  placeholder="Введите пароль"  class="form-control">
 </div>
-    <input type="submit" value="Войти"  class="btn btn-primary">
+<div class="mb-3">
+    <label for="date" class="form-label">Дата рождения: </label><input type="date" required name="date" class="form-control">
+</div>
+<div class="row p-1">
+    <div class="col-auto"><input type="submit" value="Войти"  class="btn btn-secondary"></div>
+</div>
 </form>
     </div>
 </div>
