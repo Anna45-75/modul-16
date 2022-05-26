1509ad555badc3ef4d26c592b5d6019ba15c05ef
Ann75 <76436242+Anna45-75@users.noreply.github.com>
2022-05-26 03:01:15 +0300

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,11 +1,12 @@
 <?php
 function checkPassword(string $login, string $password): bool 
-{
+{   /* подключение симуляции БД зарегистрированных users */
     $users = require __DIR__ . '/usersDB.php';
 
     foreach ($users as $user) {
         if ($user['login'] === $login 
-            && $user['password'] === $password
+            && $user['password'] === $password 
+            && $date['date'] === $date
         ) {
             return true;
         }
@@ -13,15 +14,17 @@ function checkPassword(string $login, string $password): bool
 
     return false;
 }
-
+   /* подключение кук */
 function getUserLogin(): ?string
 {
-    $loginFromCookie = $_COOKIE['login'] ?? '';
-    $passwordFromCookie = $_COOKIE['password'] ?? '';
+    $loginFromCookie = $_COOKIE['login'];
+    $passwordFromCookie = $_COOKIE['password'];
+    $dateFromCookie = $_COOKIE['date'];
 
-    if (checkPassword($loginFromCookie, $passwordFromCookie)) {
+    if (checkPassword($loginFromCookie, $passwordFromCookie, $dateFromCookie)) {
         return $loginFromCookie;
     }
 
     return null;
 }
+
